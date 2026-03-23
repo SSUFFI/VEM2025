84add5bbb98d3bdee7147f7db028163f2be0274e
SSUFFI <gkruddlsmsqk@naver.com>
2026-03-23 17:34:48 +0900

    3_23 타격이펙트 애니메이션 적용

--- Assets/Scripts/Battle/Visuals/Damage.cs
+++ Assets/Scripts/Battle/Visuals/Damage.cs
@@ -1,8 +1,7 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using DG.Tweening;
 
 public class Damage : MonoBehaviour
 {
@@ -28,10 +27,10 @@ public class Damage : MonoBehaviour
         GetComponent<CardOrder>().SetOrder(1000);
         damageTMP.text = $"-{damage}";
 
-        Sequence sequence = DOTween.Sequence()
-            .Append(transform.DOScale(Vector3.one * 0.5f, 0.5f).SetEase(Ease.InOutBack))
-            .AppendInterval(1.2f)
-            .Append(transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InOutBack))
-            .OnComplete(() => Destroy(gameObject));
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
